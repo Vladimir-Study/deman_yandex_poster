fbe7cabeba82a36965a43c5c09de1ad0001a787c
Vladimir Pavlov <volodia.pavloff201343@gmail.com>
2024-08-14 13:21:27 +0300

    add frontend

--- views.py
+++ views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.template import loader
 
 def show_map(request):
-    template = loader.get_template('map.html')
+    template = loader.get_template('index.html')
     context = {}
     renderer_page = template.render(context, request)
     return HttpResponse(renderer_page)
