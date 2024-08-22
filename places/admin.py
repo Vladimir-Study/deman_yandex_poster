513b37d32b167e8195005da22dd8d6b262116afe
Vladimir Pavlov <volodia.pavloff201343@gmail.com>
2024-08-22 15:02:51 +0300

    add inline model placesImage
    added in admin model Place

--- places/admin.py
+++ places/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 from .models import PlaceImage, Place
 
 
-@admin.register(Place, PlaceImage)
+class PlaceImagesAdmin(admin.TabularInline):
+    model = PlaceImage
+
+
+@admin.register(Place)
 class AdminPlace(admin.ModelAdmin):
     empty_value_display = "-empty-"
+    inlines = [
+        PlaceImagesAdmin,
+    ]
