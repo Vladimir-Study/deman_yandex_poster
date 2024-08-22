6839c707dc42dbace7b5c241dd17ad264e68ad1f
Vladimir Pavlov <volodia.pavloff201343@gmail.com>
2024-08-22 15:21:17 +0300

    add preview foto on the add in admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html, mark_safe
 
 from .models import PlaceImage, Place
 
 
 class PlaceImagesAdmin(admin.TabularInline):
     model = PlaceImage
+    readonly_fields = [
+        "headshot_image",
+    ]
+
+    def headshot_image(self, obj):
+        return format_html(mark_safe(f"<img src='{obj.url_img.url}' width='150'/>"))
 
 
 @admin.register(Place)
