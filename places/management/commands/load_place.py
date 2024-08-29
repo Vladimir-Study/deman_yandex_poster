128998989539352d94e84f2c6fa92570a6cd6c3e
Vladimir Pavlov <volodia.pavloff201343@gmail.com>
2024-08-29 09:44:41 +0300

    Refactored command "load_place"
    - network exceptions have been handled
    - if the place was created, a notification be shown
    - the code was optimized

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -14,23 +14,47 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         urls = options.get("urls")[0]
-        if urls:
-            for url in urls:
-                raw_data_place = requests.get(url).json()
-                new_place = Place.objects.create(
-                    title=raw_data_place.get("title"),
-                    short_description=raw_data_place.get("description_short"),
-                    long_description=raw_data_place.get("description_long"),
-                    coord_lng=raw_data_place.get("coordinates").get("lng"),
-                    coord_lat=raw_data_place.get("coordinates").get("lat"),
+        if not urls:
+            return
+        for url in urls:
+            try:
+                raw_data_place = requests.get(url)
+                if raw_data_place.status_code != 200:
+                    return
+                raw_data_place = raw_data_place.json()
+                new_place = Place.objects.get_or_create(
+                    title=raw_data_place["title"],
+                    short_description=raw_data_place.get("description_short", ""),
+                    long_description=raw_data_place.get("description_long", ""),
+                    coord_lng=raw_data_place["coordinates"]["lng"],
+                    coord_lat=raw_data_place["coordinates"]["lat"],
                 )
-                self.save_image(raw_data_place.get("imgs"), new_place)
+                if new_place[1]:
+                    self.save_image(raw_data_place.get("imgs", []), new_place[0])
+                else:
+                    print("Place was added!")
+            except requests.exceptions.HTTPError as Exc:
+                print(f"Exception {Exc} on get the place {url}")
+                continue
+            except requests.exceptions.ConnectionError as Exc:
+                print(f"Exception {Exc} on get the place {url}")
+                continue
 
     def save_image(self, images: list, model: Place):
-        if images:
-            for image in images:
-                name = image.split("/")[-1]
+        if not images:
+            return
+        for image in images:
+            name = image.split("/")[-1]
+            try:
                 payload_image_data = requests.get(image)
-                image_place = ContentFile(payload_image_data.content)
-                img = PlaceImage(place=model)
-                img.photo.save(name, image_place, save=True)
+                if payload_image_data.status_code != 200:
+                    return
+                PlaceImage(place=model).photo.save(
+                    name, ContentFile(payload_image_data.content), save=True
+                )
+            except requests.exceptions.HTTPError as Exc:
+                print(f"Exception {Exc} on get the image {image}")
+                continue
+            except requests.exceptions.ConnectionError as Exc:
+                print(f"Exception {Exc} on get the image {image}")
+                continue
